72bfd980a62323db02a1e9be164a794bf4522500
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-09-25 20:06:49 -0500

    Bite 29 -> Done
    Martin's IQ test

---
+++ Bite_29/wrong_char.py
@@ -0,0 +1,58 @@
+"""
+Martin is preparing to pass an IQ test.
+
+The most frequent task in this test is to find out 
+which one of the given characters differs from the 
+others. He observed that one char usually differs 
+from the others in being alphanumeric or not.
+
+Please help Martin! To check his answers, he needs 
+a program to find the different one (the alphanumeric 
+among non-alphanumerics or vice versa) among the given 
+characters.
+
+Complete get_index_different_char to meet this goal. 
+It receives a chars list and returns an int index 
+(zero-based).
+
+Just to be clear, 
+alphanumeric == abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
+
+Examples:
+
+['A', 'f', '.', 'Q', 2]  # returns index 2 
+(dot is non-alphanumeric among alphanumerics)
+['.', '{', ' ^', '%', 'a']  # returns index 4 
+('a' is alphanumeric among non-alphanumerics)
+See the TESTS tab for more details
+"""
+# My solution using regex
+from re import findall
+
+def get_index_different_char(chars):
+    alpha = findall(r"[a-zA-Z0-9]", ''.join(map(str, chars)))
+    non_alpha = findall(r"[^a-zA-Z0-9]", ''.join(map(str, chars)))
+
+    if len(non_alpha) < len(alpha):
+        return chars.index(non_alpha[0])
+    else:
+        if alpha[0] in '1234567890':
+            return chars.index(int(alpha[0]))
+        return chars.index(alpha[0])
+
+
+
+# Solution from pybites
+import string
+
+alphanumeric_chars = list(string.ascii_letters + string.digits)
+
+
+def get_index_different_char_1(chars):
+    matches, no_matches = [], []
+    for i, char in enumerate(chars):
+        if str(char).lower() in alphanumeric_chars:
+            matches.append(i)
+        else:
+            no_matches.append(i)
+    return matches[0] if len(matches) == 1 else no_matches[0]
