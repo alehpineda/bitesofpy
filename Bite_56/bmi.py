9299e1b50152944d6a1a58f56536456c344ed84f
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-10-04 20:30:53 -0500

    Bite 56 -> Done
    Add a command line interface to our BMI calculator

--- Bite_56/bmi.py
+++ Bite_56/bmi.py
@@ -1,3 +1,27 @@
+"""
+Complete create_parser below so that our BMI program can be called like 
+this:
+
+$ python bmi.py -h
+usage: bmi.py [-h] [-w WEIGHT] [-l LENGTH]
+
+Calculate your BMI.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -w WEIGHT, --weight WEIGHT
+                        Your weight in kg
+  -l LENGTH, --length LENGTH
+                        Your length in cm
+
+$ python bmi.py -w 80 -l 187
+Your BMI is: 22.88
+Please note that calc_bmi and handle_args are given, you only need to 
+code create_parser!
+
+We have two more Bites to practice argparse: 57 and 58.
+"""
+
 import argparse
 
 
@@ -13,7 +37,10 @@ def create_parser():
     """TODO:
        Create an ArgumentParser adding the right arguments to pass the tests,
        returns a argparse.ArgumentParser object"""
-    pass
+    parser = argparse.ArgumentParser(description = 'Calculate your BMI.')
+    parser.add_argument('-w', '--weight', type=float, help='Your weight in kg')
+    parser.add_argument('-l', '--length', type=float, help='Your length in cm')
+    return parser
 
 
 def handle_args(args=None):
