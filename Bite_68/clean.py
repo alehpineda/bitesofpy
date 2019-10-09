348af1759ecd2fafc785f8b032f6545c1c5dde62
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-10-08 20:51:52 -0500

    Bite 68 -> Done
    Remove punctuation characters from a string

---
+++ Bite_68/clean.py
@@ -0,0 +1,12 @@
+from string import punctuation
+
+def remove_punctuation(input_string):
+    """Return a str with punctuation chars stripped out"""
+    
+    return ''.join([char for char in input_string if char not in punctuation])
+
+# pybites solution
+def remove_punctuation_1(input_string):
+    """Return a str with punctuation chars stripped out"""
+    table = str.maketrans({key: None for key in punctuation})
+    return input_string.translate(table)
