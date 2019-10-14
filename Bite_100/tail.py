721c0c14b98f08ef1cd19e1a2a78b295fe2f5d67
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-10-14 18:33:14 -0500

    Bite 100 -> Done
    Display the last part of a file (Unix tail)

---
+++ Bite_100/tail.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+def tail(filepath, n):
+    """Similate Unix' tail -n, read in filepath, parse it into a list,
+       strip newlines and return a list of the last n lines"""
+    
+    with open(filepath) as f:
+        lines = f.read().splitlines()
+
+    d = deque(maxlen=n)
+    d.extend(lines)
+
+    return list(d)
+
+# pybites solution
+def tail1(filepath, n):
+    """Similate Unix' tail -n, read in filepath, parse it into a list,
+       strip newlines and return a list of the last n lines"""
+    with open(filepath) as f:
+        return [line.strip() for line in f.readlines()[-n:]]
