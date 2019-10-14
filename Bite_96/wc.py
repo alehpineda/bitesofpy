37a06cc7d35312c69269a37319d1d5ad433dede0
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-10-13 23:41:17 -0500

    Bite 96 -> Done
    Build Unix' wc program in Python

---
+++ Bite_96/wc.py
@@ -0,0 +1,31 @@
+def wc(file_):
+    """Takes an absolute file path/name, calculates the number of
+       lines/words/chars, and returns a string of these numbers + file, e.g.:
+       3 12 60 /tmp/somefile
+       (both tabs and spaces are allowed as separator)"""
+
+    with open(file_) as f:
+        words = 0
+        chars = 0
+        for lines, line in enumerate(f, 1):
+            wordlist = line.split()
+            words += len(wordlist)
+            chars += len(line)
+    
+    return f"{lines}\t{words}\t{chars}\t{file_}"
+
+
+# pybites solution
+
+def wc1(file_):
+    """Takes an absolute file path/name, calculates the number of
+       lines/words/chars, and returns a string of these numbers + file, e.g.:
+       3 12 60 /tmp/somefile
+       (both tabs and spaces are allowed as separator)"""
+    with open(file_) as f:
+        content = f.read()
+        num_lines = len(content.split('\n'))
+        num_words = len(content.split())
+        num_chars = len(content)
+        numbers = f'{num_lines}\t{num_words}\t{num_chars}'
+        return f'{numbers} {file_}'
