39c69892ec9ba839b3780daf2c7643f89fe64846
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-10-31 14:55:36 -0600

    Update convert.py
    Added pybites solution

--- 169/convert.py
+++ 169/convert.py
@@ -14,3 +14,25 @@ def convert(value: float, fmt: str) -> float:
     if fmt.lower() == 'in':
         convert = value / 2.54
     return round(convert, 4)
+
+# Pybites solution
+
+FORMATS = {"cm": 2.54, "in": 0.393_700_787_4}
+
+
+def convert1(value: float, fmt: str) -> float:
+    """Converts the value to the designated format.
+
+    :param value: The value to be converted must be numeric or raise a TypeError
+    :param fmt: String indicating format to convert to
+    :return: Float rounded to 4 decimal places after conversion
+    """
+    fmt = fmt.lower()
+
+    if fmt not in FORMATS.keys():
+        raise ValueError(f"{fmt} is an unsupported format!")
+
+    try:
+        return round(value * FORMATS[fmt], 4)
+    except TypeError as e:
+        raise TypeError(f"{e} must be a numeric!")
