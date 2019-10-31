3d901e6f6146bf80dc5d4b7996adcb4620e1235a
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-10-31 16:33:14 -0600

    Update cars.py

--- 130/cars.py
+++ 130/cars.py
@@ -1,5 +1,4 @@
 from collections import Counter
-import json
 
 import requests
 
@@ -26,3 +25,19 @@ def get_models(automaker, year):
     return {car.get('model') for car in data 
         if car.get('automaker') == automaker and car.get('year') == year}
 
+
+# Pybites solution:
+def most_prolific_automaker1(year):
+    """Given year 'year' return the automaker that released
+       the highest number of new car models"""
+    cnt = Counter(row["automaker"] for row in data
+                  if row["year"] == year).most_common()
+    return cnt[0][0]
+
+
+def get_models1(automaker, year):
+    """Filter cars 'data' by 'automaker' and 'year',
+       return a set of models (a 'set' to avoid duplicate models)"""
+    return set([row["model"] for row in data
+                if row["automaker"] == automaker and
+                row["year"] == year])
