7162b680c4973c8be9393600492f4fb47db635d4
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-10-31 17:28:51 -0600

    Update affiliation.py

--- 133/affiliation.py
+++ 133/affiliation.py
@@ -1,8 +1,19 @@
 import re
 
+PYBITES_LINK = 'http://www.amazon.com{}/?tag=pyb0f-20'
+
 def generate_affiliation_link(url):
     try:
         aff = re.search(r'(\/dp\/\d+\w)', url).group(1)
-        return 'http://www.amazon.com'+ aff +'/?tag=pyb0f-20'
+        return PYBITES_LINK.format(aff)
     except AttributeError:
         raise
+
+# Pybites solution
+
+
+PYBITES_LINK1 = 'http://www.amazon.com/dp/{}/?tag=pyb0f-20'
+
+def generate_affiliation_link1(url):
+    asin = url.split('dp/')[-1].split('/')[0]
+    return PYBITES_LINK1.format(asin)
