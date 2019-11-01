3ed281fd5e9ffa7d3c39b1ccab9de97cb8aca760
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-10-31 21:09:12 -0600

    Update words.py

--- 149/words.py
+++ 149/words.py
@@ -7,3 +7,15 @@ def sort_words_case_insensitively(words):
     number = sorted([number for number in words if number[0].isdigit()], key=str.lower)
     return word+number
 
+
+# Pybites solution
+
+
+def sort_words_case_insensitively1(words):
+    """Sort the provided word list ignoring case, and numbers last
+       (1995, 19ab = numbers / Happy, happy4you = strings, hence for
+        numbers you only need to check the first char of the word)
+    """
+    # this works because: >>> sorted([True, False])
+    # [False, True]
+    return sorted(words, key=lambda x: (x[0].isdigit(), str(x).lower()))
