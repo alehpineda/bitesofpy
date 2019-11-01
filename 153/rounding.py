f9644e3fe02cbb396071842c0b78d577d6e60d6e
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-10-31 21:24:38 -0600

    Update rounding.py
    Added Pybites solution

--- 153/rounding.py
+++ 153/rounding.py
@@ -8,3 +8,15 @@ def round_up_or_down(transactions, up=True):
    if up:
       return [ceil(transaction) for transaction in transactions]
    return [floor(transaction) for transaction in transactions]
+
+
+# Pybites solution
+
+
+def round_up_or_down1(transactions, up=True):
+   """Round the list of transactions passed in.
+       If up=True (default) ronud up, else round down.
+       Return a new list of rounded values
+   """
+   func = ceil if up else floor
+   return [func(t) for t in transactions]
