c2f46904759cdb756b9396d708e455bd61e768e5
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-11-01 18:46:16 -0600

    Bite 176 -> Done

--- 176/chessboard.py
+++ 176/chessboard.py
@@ -2,6 +2,9 @@ WHITE, BLACK = ' ', '#'
 
 
 def create_chessboard(size=8):
-    """Create a chessboard with of the size passed in.
-       Don't return anything, print the output to stdout"""
-    pass
+   """Create a chessboard with of the size passed in.
+      Don't return anything, print the output to stdout"""
+   size = int(size / 2)
+   for _ in range(size):
+      print((WHITE+BLACK)*size)
+      print((BLACK+WHITE)*size)
