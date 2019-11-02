308ddccf0481e0fd261cd719353cb11075e96357
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-11-01 18:48:45 -0600

    Update chessboard.py

--- 176/chessboard.py
+++ 176/chessboard.py
@@ -8,3 +8,22 @@ def create_chessboard(size=8):
    for _ in range(size):
       print((WHITE+BLACK)*size)
       print((BLACK+WHITE)*size)
+
+
+# Pybites solution
+
+
+def create_chessboard1(size=8):
+   """Create a chessboard with of the size passed in.
+      Don't return anything, print the output to stdout"""
+   board = ''
+
+   for i in range(size):
+      for j in range(size):
+         if (i + j) % 2 == 0:
+            board += ' '
+         else:
+            board += '#'
+      board += '\n'
+
+   print(board)
