050df457ea78be82c6a57faa088313cfcbc614f5
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-11-14 12:11:47 -0600

    Bite 113 -> Done

---
+++ 113/non_ascii.py
@@ -0,0 +1,14 @@
+def extract_non_ascii_words(text):
+    """Filter a text returning a list of non-ascii words"""
+    words = text.split()
+    return [word for word in words if _is_ascii(word)]
+
+# Pybites solution
+def _is_ascii(word):
+    """Helper to determine if a word consists of only ascii chars"""
+    return not all(ord(char) < 128 for char in word)
+
+
+def extract_non_ascii_words1(text):
+    """Filter a text returning a list of non-ascii words"""
+    return [word for word in text.split() if _is_ascii(word)]
