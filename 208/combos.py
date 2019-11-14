29dd8f1b2f313f4b45611ea4c24a5ec5fc13df3c
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-11-13 20:40:51 -0600

    Bite 208 -> Done

---
+++ 208/combos.py
@@ -0,0 +1,25 @@
+def find_number_pairs(numbers, N=10):
+    combos = []
+    pair = {}
+    
+    for i, num in enumerate(numbers):
+        diff = round(N - num, 2)
+        if diff in pair:
+            combos.append((num, diff))
+        else:
+            pair[num] = i
+
+    return list(set(_sort_all(combos)))
+
+def _sort_all(ret):
+    return sorted([tuple(sorted(n)) for n in ret])
+
+
+# Pybites solution
+
+from itertools import combinations
+
+
+def find_number_pairs1(numbers, N=10):
+    return [(i, j) for i, j in combinations(numbers, 2)
+            if i + j == N]
