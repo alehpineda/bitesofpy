5710408d382a1145f5547f61eed39e6fe1fe59fe
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-11-14 00:45:13 -0600

    Bite 209 -> Done

--- 209/docstring.py
+++ 209/docstring.py
@@ -1,3 +1,12 @@
-def sum_numbers(numbers):
-    """Write your docstring here ..."""
+from typing import List
+
+def sum_numbers(numbers: List[int]) -> int:
+    """Sums numbers
+    
+    :param numbers: a list of numbers
+    :type numbers: list
+    :raises TypeError: if not all numeric values passed in
+    :return: sum of numbers
+    :rtype: int
+    """
     pass
--- 209/test_docstring.py
+++ 209/test_docstring.py
@@ -1,1 +1,24 @@
-# tests are hidden for this Bite
+import re
+import typing
+
+from docstring import sum_numbers
+
+
+def test_sum_numbers():
+    doc = f'\n{sum_numbers.__doc__.strip()}'
+
+    # for some lines allow variable content after colon
+    for line in (r'Sums numbers',
+                 r'    :param numbers: \S.*?\n',
+                 r'    :type numbers: list',
+                 r'    :raises TypeError: \S.*?\n',
+                 r'    :return: \S.*?\n',
+                 r'    :rtype: int'):
+        # newline to test proper indenting
+        assert re.search(rf'\n{line}', doc)
+
+
+def test_sum_numbers_annotatios():
+    res = sum_numbers.__annotations__
+    assert res.get('numbers') == typing.List[int]
+    assert res.get('return') == int
