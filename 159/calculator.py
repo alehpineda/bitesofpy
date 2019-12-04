8cda35b9d838cfe86e33d6128e6cc6eeb4cf1069
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-12-04 17:22:26 -0500

    Bite 159 -> Done

---
+++ 159/calculator.py
@@ -0,0 +1,31 @@
+import operator
+
+CALCULATIONS = {
+   '+': operator.add,
+   '-': operator.sub,
+   '*': operator.mul,
+   '/': operator.truediv
+}
+
+def simple_calculator(calculation):
+   """Receives 'calculation' and returns the calculated result,
+
+      Examples - input -> output:
+      '2 * 3' -> 6
+      '2 + 6' -> 8
+
+      Support +, -, * and /, use "true" division (so 2/3 is .66
+      rather than 0)
+      
+      Make sure you convert both numbers to ints.
+      If bad data is passed in, raise a ValueError.
+   """
+
+   try:
+      num1 , sign, num2 = calculation.split()
+      return CALCULATIONS[sign](int(num1), int(num2))
+   
+
+   except (ValueError,KeyError, ZeroDivisionError) as error:
+      print(error)
+      raise ValueError
