83fcc1d397fbeff5668e2226c472c98e2e020e92
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-12-05 20:09:29 -0500

    Bite -> 175 done
    Using pandas saved a lot of time and coding

--- 175/missing_dates.py
+++ 175/missing_dates.py
@@ -1,10 +1,14 @@
+import pandas as pd
+
 def get_missing_dates(dates):
-    """Receives a range of dates and returns a sequence
-       of missing datetime.date objects (no worries about order).
+   """Receives a range of dates and returns a sequence
+      of missing datetime.date objects (no worries about order).
+      
+      You can assume that the first and last date of the
+      range is always present (assumption made in tests).
 
-       You can assume that the first and last date of the
-       range is always present (assumption made in tests).
-
-       See the Bite description and tests for example outputs.
-    """
-    pass
+      See the Bite description and tests for example outputs.
+   """
+   start, *_, end = sorted(dates)
+   complete_dates = pd.date_range(start=start, end=end).date
+   return list(set(dates) ^ set(complete_dates))
