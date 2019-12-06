2d55c44e0e2e30ec31b43a395d47468e633ca29b
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-12-06 17:35:46 -0500

    Update duplicates.py
    Added pybites solution. Without Counter

--- 118/duplicates.py
+++ 118/duplicates.py
@@ -14,3 +14,18 @@ def get_duplicate_indices(words):
    return sorted([words.index(word) 
                   for word, value in Counter(words).most_common() 
                   if value > 1])
+
+
+# Pybites solution
+
+def get_duplicate_indices1(words):
+    """Given a list of words, loop through the words and check for each
+       word if it occurs more than once.
+       If so return the index of its first ocurrence.
+       For example in the following list 'is' and 'it'
+       occurr more than once, and they are at indices 0 and 1 so you would
+       return [0, 1]:
+       ['is', 'it', 'true', 'or', 'is', 'it', 'not?'] => [0, 1]
+       Make sure the returning list is unique and sorted in ascending order."""
+    duplicate_words = {word for word in words if words.count(word) > 1}
+    return sorted([words.index(word) for word in duplicate_words])
