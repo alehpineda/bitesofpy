4ee3f17a9a9810c4f20b25e9912987c81caa1974
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-12-05 20:16:37 -0500

    Update missing_dates.py
    Added pybites solution

--- 175/missing_dates.py
+++ 175/missing_dates.py
@@ -12,3 +12,25 @@ def get_missing_dates(dates):
    start, *_, end = sorted(dates)
    complete_dates = pd.date_range(start=start, end=end).date
    return list(set(dates) ^ set(complete_dates))
+
+
+# Pybites solution
+
+from dateutil import rrule
+
+
+def get_missing_dates1(dates):
+    """Receives a range of dates and returns a sequence
+       of missing datetime.date objects (no worries about order).
+
+       You can assume that the first and last date of the
+       range is always present (assumption made in tests).
+
+       See the Bite description and tests for example outputs.
+    """
+    start = min(dates)
+    count = max(dates).day - start.day
+    date_range = [d.date() for d in
+                  rrule.rrule(rrule.DAILY, count=count,
+                              dtstart=start)]
+    return set(date_range) - set(dates)
