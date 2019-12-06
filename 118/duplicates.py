47015e080c370787ec07117b28f0feef726aaea4
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-12-06 17:33:55 -0500

    Bite 118 -> Done

--- 118/duplicates.py
+++ 118/duplicates.py
@@ -1,10 +1,16 @@
+from collections import Counter
+
 def get_duplicate_indices(words):
-    """Given a list of words, loop through the words and check for each
-       word if it occurs more than once.
-       If so return the index of its first ocurrence.
-       For example in the following list 'is' and 'it'
-       occurr more than once, and they are at indices 0 and 1 so you would
-       return [0, 1]:
-       ['is', 'it', 'true', 'or', 'is', 'it', 'not?'] => [0, 1]
-       Make sure the returning list is unique and sorted in ascending order."""
-    pass
+   """Given a list of words, loop through the words and check for each
+      word if it occurs more than once.
+      If so return the index of its first ocurrence.
+      For example in the following list 'is' and 'it'
+      occurr more than once, and they are at indices 0 and 1 so you would
+      return [0, 1]:
+      ['is', 'it', 'true', 'or', 'is', 'it', 'not?'] => [0, 1]
+      Make sure the returning list is unique and sorted in ascending order.
+      count, list comprehension, set
+   """
+   return sorted([words.index(word) 
+                  for word, value in Counter(words).most_common() 
+                  if value > 1])
