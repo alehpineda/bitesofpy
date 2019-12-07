66a327b7b5c04bf68bbc00a36d0f5223e7bbd874
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-12-07 00:07:55 -0500

    Update grouping.py
    Added pybites solution, using islice

--- 222/grouping.py
+++ 222/grouping.py
@@ -1,4 +1,3 @@
-import types
 from itertools import zip_longest
 
 
@@ -15,8 +14,40 @@ def group(iterable, n):
         list: The list of groups of size n,
               where each group is a list of n elements.
   """
-  return list(list(filter(None, tup)) 
-              for tup in zip_longest(*[iter(iterable)]*n))
+  return [list(filter(None, tup))
+          for tup in zip_longest(*[iter(iterable)]*n)]
+
+
+# Pybites solution
+
+import types
+from itertools import islice
+
+
+def group1(iterable, n):
+    """Splits an iterable set into groups of size n and a group
+       of the remaining elements if needed.
+
+       Args:
+         iterable (list): The list whose elements are to be split into
+                          groups of size n.
+         n (int): The number of elements per group.
+
+       Returns:
+         list: The list of groups of size n,
+               where each group is a list of n elements.
+    """
+    # cast generator if necessary
+    if not isinstance(iterable, types.GeneratorType):
+        iterable = (i for i in iterable)
+
+    ret = []
+    while True:
+        row = list(islice(iterable, n))
+        if not row:
+            break
+        ret.append(row)
+    return ret
 
 
 if __name__ == '__main__':
