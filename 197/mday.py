ceeb060363e2b6e7c86e0e14498ec8bd052f3af6
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-12-06 23:06:07 -0500

    Update mday.py
    Added pybites solution

--- 197/mday.py
+++ 197/mday.py
@@ -6,3 +6,15 @@ def get_mothers_day_date(year):
       is celebrated assuming it's the 2nd Sunday of May."""
    
    return date(year,1,1)+relativedelta(months=+4, weekday=SU(2))
+
+
+# Pybites solution
+
+MAY = 5
+
+
+def get_mothers_day_date1(year):
+   """Given the passed in year int, return the date Mother's Day
+      is celebrated assuming it's the 2nd Sunday of May."""
+   first_of_may = date(year=year, month=MAY, day=1)
+   return first_of_may + relativedelta(weeks=1, weekday=SU)
