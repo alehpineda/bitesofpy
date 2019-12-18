249428d7dc12a05d64072805e00c6c9892321337
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-12-18 14:57:56 -0500

    Bite 70 & 228 -> Done
    Blue belt achieved

---
+++ 228/gravatar.py
@@ -0,0 +1,22 @@
+import hashlib
+
+GRAVATAR_URL = ("https://www.gravatar.com/avatar/"
+                "{hashed_email}?s={size}&r=g&d=robohash")
+
+
+def create_gravatar_url(email, size=200):
+   """Use GRAVATAR_URL above to create a gravatar URL.
+
+      You need to create a hash of the email passed in.
+
+      PHP example: https://en.gravatar.com/site/implement/hash/
+
+      For Python check hashlib check out (md5 / hexdigest):
+      https://docs.python.org/3/library/hashlib.html#hashlib.hash.hexdigest
+   """
+   #email = email.lower().strip().encode('utf-8')
+   #hashed_email = hashlib.md5(email).hexdigest()
+   #return GRAVATAR_URL.format(hashed_email=hashed_email, size=size)
+   m = hashlib.md5()
+   m.update(email.lower().strip().encode())
+   return GRAVATAR_URL.format(hashed_email=m.hexdigest(), size=size)
---
+++ 70/eggs.py
@@ -0,0 +1,21 @@
+from random import choice
+
+COLORS = 'red blue green yellow brown purple'.split()
+
+
+class EggCreator:
+    """Iterator that counts upward eggs."""
+
+    def __init__(self, limit):
+        self.limit = limit
+        self.num = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.num += 1
+        if self.num > self.limit:
+            raise StopIteration('Limit exceeded')
+        else:
+            return f'{choice(COLORS)} egg'
