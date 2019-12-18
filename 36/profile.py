a17e8b435ed04ff7cf45c0c2f59708ae18df0a7f
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-12-18 12:45:57 -0500

    Update profile.py
    Updated solution

--- 36/profile.py
+++ 36/profile.py
@@ -3,11 +3,13 @@ def get_profile(name, age, *sports, **awards):
         raise ValueError('Age not a number')
     if len(sports) > 5:
         raise ValueError('At least 5 sports')
-    if not sports and not awards:
-        return {'name': name, 'age': age}
-    elif not sports and awards:
-        return {'name': name, 'age': age, 'awards': {**awards}}
-    elif sports and not awards:
-        return {'name': name, 'age': age, 'sports': sorted([*sports])}
-    else:
-        return {'name':name, 'age':age, 'sports':sorted([*sports]), 'awards': {**awards}}
+
+    profile = {'name': name, 'age': age}
+    
+    if sports:
+        profile['sports'] = sorted(sports)
+    
+    if awards:
+        profile['awards'] = awards
+    
+    return profile
