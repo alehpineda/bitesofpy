2bb4fd6b1b26fd5bee794a45a7efb4b498ed2d1d
Alejandro H Pineda <ale.hpineda@gmail.com>
2020-01-07 16:59:27 -0500

    Bite 120 -> Done

--- 120/validate.py
+++ 120/validate.py
@@ -4,3 +4,11 @@ from functools import wraps
 def int_args(func):
     @wraps(func)
     # complete this decorator
+    def wrapper(*args):
+        if not all(isinstance(x, int) for x in args):
+            raise TypeError
+        if any(x < 0 for x in args):
+            raise ValueError
+        else:
+            return func(*args)
+    return wrapper
