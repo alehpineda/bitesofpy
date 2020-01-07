5b0d8ef27df407212004ee3292ca5980e3c32d64
Alejandro H Pineda <ale.hpineda@gmail.com>
2020-01-07 16:42:26 -0500

    Bite 122 -> Done

--- 122/anagram.py
+++ 122/anagram.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def is_anagram(word1, word2):
     """Receives two words and returns True/False (boolean) if word2 is
        an anagram of word1, ignore case and spacing.
@@ -8,3 +11,11 @@ def is_anagram(word1, word2):
 
 def _clean_word(word):
     return word.strip().replace(' ', '').lower()
+
+
+def is_anagram1(word1, word2):
+    """Receives two words and returns True/False (boolean) if word2 is
+       an anagram of word1, ignore case and spacing.
+       About anagrams: https://en.wikipedia.org/wiki/Anagram"""
+    word1, word2 = _clean_word(word1), _clean_word(word2)
+    return Counter(word1) == Counter(word2)
