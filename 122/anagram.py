7a32a3b0d3be764a548c6006d9258612b1257b3d
Alejandro H Pineda <ale.hpineda@gmail.com>
2020-01-07 16:12:13 -0500

    Bite 122 -> Done

--- 122/anagram.py
+++ 122/anagram.py
@@ -2,4 +2,9 @@ def is_anagram(word1, word2):
     """Receives two words and returns True/False (boolean) if word2 is
        an anagram of word1, ignore case and spacing.
        About anagrams: https://en.wikipedia.org/wiki/Anagram"""
-    pass
+    word1, word2 = _clean_word(word1), _clean_word(word2)
+    return set(word1) == set(word2) and len(word1) == len(word2)
+
+
+def _clean_word(word):
+    return word.strip().replace(' ', '').lower()
