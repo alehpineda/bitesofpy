a48f58ae6d44ffac1bf537ee906ecd54758bb250
Alejandro H Pineda <ale.hpineda@gmail.com>
2020-01-08 13:44:48 -0500

    Bite 219 -> Done

---
+++ 219/notifications.py
@@ -0,0 +1,11 @@
+from datetime import date, timedelta
+
+TODAY = date.today()
+
+
+def gen_bite_planning(num_bites=1, num_days=1, start_date=TODAY):
+    days = 0
+    while True:
+        days += num_days
+        for _ in range(num_bites):
+            yield start_date + timedelta(days=days)
