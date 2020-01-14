c28caa8bc9bb8d6eb6eba466830d7834c0b8817c
Alejandro H Pineda <ale.hpineda@gmail.com>
2020-01-14 15:44:45 -0500

    Bite 230 -> Done

--- 230/thumbs.py
+++ 230/thumbs.py
@@ -2,4 +2,17 @@ THUMBS_UP, THUMBS_DOWN = '👍', '👎'
 
 
 class Thumbs:
-    pass
+    def __mul__(self, other):
+        if other == 0:
+            raise ValueError()
+        elif 0 < other < 4:
+            return THUMBS_UP * other
+        elif other > 3:
+            return f"{THUMBS_UP} ({other}x)"
+        elif -4 < other < 0:
+            return THUMBS_DOWN * -other
+        elif other < -3:
+            return f"{THUMBS_DOWN} ({-other}x)"
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
