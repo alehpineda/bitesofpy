0637678d6da976c8e438c21728211b1b4c901a37
Alejandro H Pineda <ale.hpineda@gmail.com>
2020-01-17 12:37:38 -0500

    Update hundred_days.py
    Added pybites solution

--- 147/hundred_days.py
+++ 147/hundred_days.py
@@ -1,4 +1,5 @@
 from datetime import date, timedelta
+from dateutil.rrule import rrule, DAILY, MO, TU, WE, TH, FR
 
 TODAY = date(year=2018, month=11, day=29)
 
@@ -14,3 +15,14 @@ def get_hundred_weekdays(start_date=TODAY):
             hundred.append(start_date + timedelta(days=days))
         days += 1
     return hundred
+
+
+# Pybites solution using dateutil
+def get_hundred_weekdays1(start_date=TODAY):
+    """Return a list of hundred date objects starting from
+       start_date up till 100 weekdays later, so +100 days
+       skipping Saturdays and Sundays"""
+    date_range = rrule(DAILY, count=100,
+                       byweekday=(MO, TU, WE, TH, FR),
+                       dtstart=start_date)
+    return [dt.date() for dt in date_range]
