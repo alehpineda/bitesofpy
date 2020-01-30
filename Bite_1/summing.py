c5ff8170b4460100bab195304ad86c11664cd71d
Alejandro Pineda <ale.hpineda@gmail.com>
2020-01-30 13:06:46 -0600

    desde windows

--- Bite_1/summing.py
+++ Bite_1/summing.py
@@ -1,6 +1,6 @@
 def sum_numbers(numbers=None):
     if numbers == []:
         return 0
-    if numbers == None:
+    if numbers is None:
         return sum(list(range(101)))
     return sum(numbers)
