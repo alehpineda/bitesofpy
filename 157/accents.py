ac3e3be5be87134236f294994f91c7dd88136ad5
Alejandro Pineda <ale.hpineda@gmail.com>
2020-02-01 17:40:44 -0600

    Bite 157 -> Done

---
+++ 157/accents.py
@@ -0,0 +1,10 @@
+import unicodedata
+
+
+def filter_accents(text):
+    """Return a sequence of accented characters found in
+       the passed in lowercased text string
+    """
+    # decomposition return base char + added symbol or ''
+    # you could also use unicodedata.normalize
+    return (char for char in text.lower() if unicodedata.decomposition(char))
