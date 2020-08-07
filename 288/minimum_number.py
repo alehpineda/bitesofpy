785cd27f456fad672d924a22bacae20bfd5887a0
Alejandro Pineda <ale.hpineda@gmail.com>
2020-08-07 18:18:35 -0500

    Bite 288 - Done

--- 288/minimum_number.py
+++ 288/minimum_number.py
@@ -2,4 +2,7 @@ from typing import List
 
 
 def minimum_number(digits: List[int]) -> int:
-    ...
+    if not digits:
+        return 0
+
+    return int("".join(map(str, sorted(list(set(digits))))))
