f058f61a5986a5f2b01cb5eb1548d931254f1141
Alejandro Pineda <ale.hpineda@gmail.com>
2020-08-07 18:22:00 -0500

    updated 288

--- 288/minimum_number.py
+++ 288/minimum_number.py
@@ -2,7 +2,4 @@ from typing import List
 
 
 def minimum_number(digits: List[int]) -> int:
-    if not digits:
-        return 0
-
-    return int("".join(map(str, sorted(list(set(digits))))))
+    return int("".join(map(str, sorted(list(set(digits)))))) if digits else 0
