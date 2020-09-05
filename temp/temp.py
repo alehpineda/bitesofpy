e6290e86f1dc29a82f94fbb20ce457ecb2109774
Alejandro Pineda <ale.hpineda@gmail.com>
2020-09-05 13:23:51 -0500

    Update temp.py

--- temp/temp.py
+++ temp/temp.py
@@ -1,20 +1,8 @@
-import requests
-from requests import HTTPError, Timeout
+for e in range(9):
+    if e % 3 == 0:
+        print(e)
 
+e = [e for e in range(9) if e % 3 == 0]
+print(e)
 
-def google_query(query):
-    """
-    trivial function that does a GET request
-    against google, checks the status of the
-    result and returns the raw content
-    """
-    try:
-        url = "https://www.google.com"
-        params = {"q": query}
-        resp = requests.get(url, params=params, timeout=1)
-        resp.raise_for_status()
-        return resp.content
-    except HTTPError:
-        return False
-    except Timeout:
-        return True
+e = (e for e in range(9) if e % 3 == 0)
